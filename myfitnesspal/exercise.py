69daa4ceb297b50fbd6e74ac960817d7aa271bb6
serge_katzmann <serge@katsmann.de>
2018-08-02 18:53:01 +0200

    Simple extension to read exercise data. This first version is only retrieving the daily total burned calories amount.

--- myfitnesspal/exercise.py
+++ myfitnesspal/exercise.py
@@ -1,11 +1,31 @@
-class Exercise():
-    def __init__(self, burnedCalories):
-        self._burnedCalories = burnedCalories
+from myfitnesspal.base import MFPBase
+
+
+class Exercise(MFPBase):
+    def __init__(self, name, entries):
+        self._name = name
+        self._entries = entries
+
+    def __getitem__(self, value):
+        if not isinstance(value, int):
+            raise ValueError('Index must be an integer')
+        return self.entries[value]
+
+    def __len__(self):
+        return len(self.entries)
 
     @property
-    def burnedCalories(self):
-        if(self._burnedCalories == None):
-            return "0"
-        else:
-            return self._burnedCalories
+    def entries(self):
+        return self._entries
 
+    @property
+    def name(self):
+        return self._name
+
+    def get_as_list(self):
+        return [e.get_as_dict() for e in self.entries]
+
+    def __unicode__(self):
+        return u'%s' % (
+            self.name.title()
+        )
